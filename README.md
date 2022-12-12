af738d8b77903f789f3862991fba73b1739cf0dc
AdvancedCube <sciencekill@outlook.com>
2022-12-12 12:41:07 +0800

    More-Tools

---
+++ README.md
@@ -0,0 +1,3 @@
+# More-Tools仓库
+## 使用方式：直接克隆仓库，打开More-Tools.exe。
+### 进阶操作：Install-Libs文件夹中，安装依赖库，用Python执行源文件
---
+++ old_source_code/Main.py
@@ -0,0 +1,41 @@
+#导入第三方库
+import ttkbootstrap as ttk#ttkbootstrap
+import os#os
+import sys#sys
+import datetime
+import subprocess
+#os.system("@echo off")
+#定义函数
+def startmmp():
+    #os.system(".\\Python_Release\\MMP.exe")
+    subprocess.Popen(".\\Python_Release\\MMP.exe")
+def startccp():
+    #os.system(".\\CPP_Release\\Caesar_Code_Part.exe")
+    subprocess.Popen(".\\CPP_Release\\Caesar_Code_Part.exe")
+def exitprogram():
+    sys.exit()
+#主程序
+mw=ttk.Window(themename='superhero')#mw窗口定义
+mw.iconbitmap(".\\ICO\\MT-ICON.ico")#定义窗口图标
+mw.title("More Tools")#mw标题
+mw.geometry("800x600")#mw窗口尺寸
+mainlabel=ttk.Label(mw,style="primary",text="欢迎来到More Tools",font=("Frutiger",24)).grid(column=0,row=0,padx=5,pady=5)
+tiplabel=ttk.Label(mw,style="primary",text="我们有如下几款产品:").grid(column=0,row=1,padx=5,pady=5)
+mmpbutton=ttk.Button(mw,style="success-outline",text="More Message:一款卓越的刷屏器",width=40,command=startmmp).grid(column=0,row=2,padx=5,pady=5)
+tp1button=ttk.Button(mw,style="success-outline",text="凯撒密码工具",width=40,command=startccp).grid(column=0,row=3,padx=5,pady=5)
+tp2button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=0,row=4,padx=5,pady=5)
+tp3button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=0,row=5,padx=5,pady=5)
+tp4button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=0,row=6,padx=5,pady=5)
+tp5button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=0,row=7,padx=5,pady=5)
+tp6button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=0,row=8,padx=5,pady=5)
+tp7button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=0,row=9,padx=5,pady=5)
+tp8button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=2,padx=5,pady=5)
+tp9button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=3,padx=5,pady=5)
+tp10button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=4,padx=5,pady=5)
+tp11button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=5,padx=5,pady=5)
+tp12button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=6,padx=5,pady=5)
+tp13button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=7,padx=5,pady=5)
+tp14button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=8,padx=5,pady=5)
+tp15button=ttk.Button(mw,style="success-outline",text="敬请期待",width=40).grid(column=1,row=9,padx=5,pady=5)
+exitbutton=ttk.Button(mw,style="danger-outline",text="退出",width=20,command=exitprogram).grid(column=1,row=14,padx=5,pady=5)
+mw.mainloop()#保持mw窗口
